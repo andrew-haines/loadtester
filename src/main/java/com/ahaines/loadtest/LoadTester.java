44d1cb78625c4e5aa09c7d7c69e3961bed8a04d2
Andrew Haines <ajh21@sussex.ac.uk>
2013-05-07 13:17:06 +0100

    Renaming group id to loadtester-api

--- src/main/java/com/ahaines/loadtest/LoadTester.java
+++ src/main/java/com/ahaines/loadtest/LoadTester.java
@@ -1,7 +1,6 @@
 package com.ahaines.loadtest;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
--- src/main/java/com/ahaines/loadtest/report/AggregatedMessage.java
+++ src/main/java/com/ahaines/loadtest/report/AggregatedMessage.java
@@ -1,7 +1,6 @@
 package com.ahaines.loadtest.report;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
